8acfe7fd318f843bfd6033f26940b3d0e4bf5620
Augusto Souza <augustodesouzacardoso@gmail.com>
2021-04-24 16:24:08 -0300

    created bookmarks actions

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,33 @@
 class BookmarksController < ApplicationController
+  before_action :find_list, except: [:destroy]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.restaurant = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie, :list)
+  end
+
+  def find_list
+    @list = List.find(params[:list_id])
+  end
 end
